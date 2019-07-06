9d3c26843dda0971f37cdb5b98d7497b20d74e88
maidoushaoxia <shaoyun5429@163.com>
2019-07-06 20:34:43 +0800

    队列算法

---
+++ 12-学习JavaScript数据结构与算法-读书笔记/03-队列/queue.js
@@ -0,0 +1,67 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+// const queue = new Queue();
+// console.log(queue.isEmpty()); // outputs true
+// queue.enqueue('John');
+// queue.enqueue('Jack');
+// console.log(queue.toString()); // John,Jack
+// queue.enqueue('Camila');
+// console.log(queue.toString()); // John,Jack,Camila
+// console.log(queue.size()); // outputs 3
+// console.log(queue.isEmpty()); // outputs false
+// queue.dequeue(); // remove John
+// queue.dequeue(); // remove Jack
+// console.log(queue.toString()); // Camila
