2103f2597deae8ed3aa24929fc0d83ba58b09926
maidoushaoxia <shaoyun5429@163.com>
2019-08-18 12:37:37 +0800

    桶排序的算法，修改插入排序

---
+++ 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/bucketSort.js
@@ -0,0 +1,77 @@
+/*
+ * @Author: shaoyun
+ * @Date: 2019-08-17 20:36:27
+ * @LastEditors: shaoyun
+ * @LastEditTime: 2019-08-18 12:37:19
+ * @Description: 桶排序的算法
+ * 首先找出数组的最小值和最大值，并计算桶的数量
+ */
+/**
+ * @description: 复杂度为O(n+k)
+ * @param {arr[]} 
+ * @param {number} bucketSize // 每个桶的区间
+ * @return: 
+ */
+function bucketSort (arr, bucketSize) {
+  debugger
+  // 找到最小值和最大值
+  let minValue = arr[0]
+  let maxValue = arr[0]
+  arr.forEach(item => {
+    if (item < minValue) {
+      minValue = item
+    }else if (item > maxValue) {
+      maxValue = item
+    }
+  })
+
+  // 桶的数量
+  const bucketCount = Math.floor((maxValue - minValue) / bucketSize + 1)
+  // 二维数组，每一项都代表一个桶
+  let buckets = []
+  // 初始化每个桶
+  for (let i = 0;i < bucketCount;i++) {
+    buckets[i] = []
+  }
+  // 将数组中的项放到对应的桶中
+  for (let i = 0;i < arr.length;i++) {
+    let index = Math.floor((arr[i] - minValue) / bucketSize)
+    buckets[index].push(arr[i])
+  }
+  return buckets
+}
+
+/**
+ * @description: 对每个桶单独排序
+ * @param {arr[[]]} buckets
+ * @return: 
+ */
+function sortBuckets (buckets) {
+  debugger
+  let sortArr = []
+  for (let i = 0;i < buckets.length;i++) {
+    // 如果桶不为空，则对其进行插入排序
+    if (buckets[i] !== null) {
+      insertSort(buckets[i])
+    }
+    // 将排好序的桶放入排序数组中
+    sortArr.push(...buckets[i]) // ...arr可以将数组展开，这样就把数组中的项传进去了。否则push的还是数组
+  }
+  return sortArr
+}
+
+/**
+ * @description: 插入排序
+ * @param {arr[]} 
+ * @return: 
+ */
+function insertSort (arr) {
+  for (let i = 1;i < arr.length;i++) {
+    let j = i
+    while (j > 0 && arr[j-1] > arr[j]) {
+      [arr[j-1],arr[j]] = [arr[j],arr[j-1]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+      j--
+    }
+  }
+  return arr
+}
--- 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/insertSort.js
+++ 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/insertSort.js
@@ -2,7 +2,7 @@
  * @Author: shaoyun
  * @Date: 2019-08-15 20:29:55
  * @LastEditors: shaoyun
- * @LastEditTime: 2019-08-15 22:04:19
+ * @LastEditTime: 2019-08-18 12:15:58
  * @Description: 
  */
 /**
@@ -11,17 +11,13 @@
  * @return: 
  */
 function insertSort (arr) {
-  let temp
   for (let i = 1;i<arr.length;i++) {
     let j = i
-    temp = arr[i] // 保存这个要比较的值
     while (j>0 && arr[j-1] > temp) {
-      // 若前一个数比要比较的数大，则将前一个数换到后面，直到j为0
-      arr[j] = arr[j-1]
+      // 若前一个数比要比较的数大，则交换位置，直到j为0
+      [arr[j-1],arr[j]] = [arr[j],arr[j-1]]   
       j--
     }
-    // 全部比较好后，将最后一次换位置的数赋值为要比较的数
-    arr[j] = temp
   }
   return arr
 }
