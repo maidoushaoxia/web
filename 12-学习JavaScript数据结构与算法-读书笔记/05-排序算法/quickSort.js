72e1998d7bad4d279e1673fe2efd69a58b82cd3d
maidoushaoxia <shaoyun5429@163.com>
2019-09-01 19:11:37 +0800

    【*】快排修改

--- 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/quickSort.js
+++ 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/quickSort.js
@@ -2,7 +2,7 @@
  * @Author: shaoyun
  * @Date: 2019-08-17 10:58:11
  * @LastEditors: shaoyun
- * @LastEditTime: 2019-08-17 17:03:05
+ * @LastEditTime: 2019-09-01 19:10:44
  * @Description: 快速排序的算法
  * 从数组中选择一个基准，创建左指针和右指针。
  * 移动左指针，直到找到一个比基准大的；然后移动右指针，直到找到一个比基准小的；然后交换他们，并移动左右指针，直到左指针超过了右指针，则停止执行，返回左指针的索引。
@@ -18,7 +18,7 @@
 function quickSort(arr, left, right) {
   if (arr.length > 1) {
     // index为划分函数所返回的左指针的索引
-    let index = partiton(arr, left, right)
+    let index = partition(arr, left, right)
     // 若子数组存在较小值，则递归
     if (left < index) {
       quickSort(arr, left, index - 1)
@@ -44,12 +44,12 @@ function partiton(arr, left, right) {
   // 两个指针没有相遇，则进行划分操作
   while (i <= j) {
     // 找到一个大于等于基准的数时，左指针停止
-    while (arr[i] < pivo) {
+    while (arr[i] < pivot) {
       i++
     }
     // 找到一个小于等于基准的数时，右指针停止
     while (arr[j] > pivot) {
-      j++
+      j--
     }
     // 如果最终左指针没有超过右指针（即左边元素大），则交换两个元素
     if (i <= j) {
