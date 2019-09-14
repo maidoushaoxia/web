e36992777ba31818a63ed657b9c8d38c590e8a01
maidoushaoxia <shaoyun5429@163.com>
2019-09-14 14:17:34 +0800

    【*】修改选择排序

--- 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/selectionSort.js
+++ 12-学习JavaScript数据结构与算法-读书笔记/05-排序算法/selectionSort.js
@@ -2,8 +2,8 @@
  * @Author: shaoyun
  * @Date: 2019-08-14 21:09:31
  * @LastEditors: shaoyun
- * @LastEditTime: 2019-08-15 22:03:40
- * @Description: 
+ * @LastEditTime: 2019-09-14 14:17:16
+ * @Description: 选择排序
  */
 /**
  * @description: 选择排序的算法，找到数组中最小的值并放在第一位，以此类推。复杂度为O(n^2)
@@ -15,7 +15,7 @@ function selectionSort(arr) {
   let indexMin
   for (let i = 0; i < len - 1; i++) {
     indexMin = i
-    for (let j = i; j < len; j++) {
+    for (let j = i + 1; j < len; j++) {
       if (arr[j] < arr[indexMin]) {
         indexMin = j
       }
